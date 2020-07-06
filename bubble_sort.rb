3278b54fe65a9e5b04359cbcd29e9b76f9eeddeb
SigmaSam <57440278+SigmaSam@users.noreply.github.com>
2020-07-06 12:57:29 -0300

    Added boxes to delimitate strings.

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -13,6 +13,6 @@ def bubble_sort_by(array)
   print array
 end
 
-bubble_sort_by(%w(hey hi ruby hello marcos)) do |l, r|
+bubble_sort_by(%w([hey][hi][Ruby][hello][Marcos])) do |l, r|
   (l.length - r.length).positive?
 end
