ddfb855661e07cfe10dbe5926480140d9c659f3c
Marcos Hernández Campos <marcoshdezcam@gmail.com>
2020-07-07 13:27:02 -0500

    Issues pointed out by TSE solved: Add bubble_sort method && add no block case

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,20 +1,40 @@
-def bubble_sort(array)
-  swap = true
-  while swap == true
-    swap = false
-    (array.size - 1).times do |i|
-      comparator = yield array[i] , array[i + 1]
-      if comparator == (True || False) || Nil 
-        puts "No block given."
-      if comparator
-        array[i], array[i + 1] = array[i + 1], array[i]
-        swap = true
+def bubble_sort(numbers)
+  if numbers.is_a?(Array) && numbers.any?
+    swap = true
+    while swap == true
+      swap = false
+      (numbers.length - 1).times do |i|
+        if numbers[i] > numbers[i + 1]
+          numbers[i], numbers[i + 1] = numbers[i + 1], numbers[i]
+          swap = true
+        end
       end
     end
+    print numbers
+  else
+    puts 'No numbers given.'
   end
-  print array
 end
 
-bubble_sort(%w[hey hi Ruby hello Marcos]) do |l, r|
+def bubble_sort_by(array)
+  swap = true
+  if array.is_a?(Array) && array.any?
+    while swap == true
+      swap = false
+      (array.size - 1).times do |i|
+        comparator = yield array[i], array[i + 1]
+        if comparator
+          array[i], array[i + 1] = array[i + 1], array[i]
+          swap = true
+        end
+      end
+    end
+    print array
+  else
+    puts 'No block given.'
+  end
+end
+
+bubble_sort_by(%w[hey Ruby hi hello Marcos Sam]) do |l, r|
   (l.length - r.length).positive?
 end
